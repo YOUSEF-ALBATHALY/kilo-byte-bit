0052743911c8f85dd14ee4f82b361633ca199066
@Y0U$EF <yousefalialbthaly@outlook.com>
2025-07-13 14:44:23 +0300

    Added kilobyte to bit converter program

---
+++ kilo_converter.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  cout << "\n=================================\n";
+  cout << "===== kilo byte bit program =====\n";
+  cout << "=================================\n\n";
+
+  cout << "What is the number of kilobyte ?: ";
+  int kiloByte;
+  cin >> kiloByte;
+  cout << "\n\nthe number in kilo is : " << kiloByte << " kb\n";
+  int bytes = kiloByte * 1024;
+  cout << "the number in bytes is : " << bytes << " byte\n";
+  int bit = bytes * 8;
+  cout << "the number in bit is : " << bit << " bit\n";
+
+  
+  cout << "\n=================================\n";
+  cout << "=============@Y0U$EF=============\n";
+  cout << "=================================\n";
+
+  return 0;
+}
